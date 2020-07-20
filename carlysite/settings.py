79011f42bad72dc2a0f4d76effa098f424b7f36e
Ryan Keys <r.keys1998@gmail.com>
2020-07-20 00:24:31 -0700

    added docker-compose support

--- carlysite/settings.py
+++ carlysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nvzt7gd3)&=4s%@ggty0occv3s!z=u9$gu)+e@8wgeilrhbdm-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
